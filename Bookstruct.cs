01bef71811bea68435e43f7885c9ef0e23905f37
Deepali Kamatkar <deepali.kamatkar@gmail.com>
2022-01-14 15:36:29 -0500

    Add project files.

---
+++ Bookstruct.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod6Demo1
+{
+    // access specifiers : public, internal, private, protected
+     struct Author //internal ( private to namespace)
+    {
+        public string authorname;
+
+    }
+    public struct Book
+    {
+
+        // 100
+        // "FIC-2015-1001"
+        //data menmbers: private
+        //int i;
+        //data hiding
+       // string myname; //private
+        private string bookId;
+        private string name;
+        private string author;
+        private int copies;
+        private string ISBN;
+        //constructor
+        //initialize 
+        
+        public Book(string id,string name,string author,int cps,string isbn)
+        {
+            this.bookId = id;
+            this.name = name;
+            this.author = author;
+            this.copies = cps;
+            this.ISBN = isbn;
+
+        }
+        internal string Getbookname() // private to the type
+        {
+            //me
+
+            return this.name;
+        }
+        public void Setbookname(string name)
+        {
+
+            //validation logic ( len <50)
+            //business logic
+            if (name.Length <= 15)
+            {
+                this.name = name;
+            }
+            else
+            {
+                Console.WriteLine("Len should be small than 50");
+            }
+        }
+        public string Getbookid() // private to the type
+        {
+            //me
+            
+            return this.bookId;
+        }
+        public void Setbookid(string id)
+        {
+           
+
+            this.bookId=id;
+        }
+
+        public bool Checkbook(string name)
+        {
+            // search logic
+            return true;
+        }
+
+        
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace Mod6Demo1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {// structure : value type , stack 
+
+           
+            
+            Book b1 = new Book();
+            Book b2=new Book("FIC-33-RE","FF","YY",20,"sfsf");
+            
+            Console.WriteLine("Enter book details :");
+            Console.WriteLine("Enter book id:");
+            b1.Setbookid(Console.ReadLine());
+            Console.WriteLine("Enter book name:");
+            b1.Setbookname(Console.ReadLine());
+            
+            Console.WriteLine("Book details are:");
+            Console.WriteLine("Book id is: {0}",b1.Getbookid());
+            Console.WriteLine("Book name is: {0}", b1.Getbookname());
+            
+            Console.WriteLine("Second book is");
+            Console.WriteLine("Book id is: {0}", b2.Getbookid());
+            Console.WriteLine("Book name is: {0}", b2.Getbookname());
+
+            Console.ReadLine();
+            //Console.WriteLine(b1.ToString());
+
+            Author a1 = new Author();
+            
+            
+           
+
+            //constructor 
+        }
+    }
+}
